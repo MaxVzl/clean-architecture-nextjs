e03fdbdc8f06f21f1c22ad10f038cc1508e61f5c
MaxVzl <maxime.vozelle@gmail.com>
2026-03-18 21:38:34 +0100

    Remove unnecessary delay in user actions for improved performance

--- src/actions/users.actions.ts
+++ src/actions/users.actions.ts
@@ -9,14 +9,12 @@ import { actionClient } from "@/lib/safe-action";
 import { revalidatePath } from "next/cache";
 
 export const getUsersAction = actionClient.action(async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
   return await getUsersUseCase.execute();
 });
 
 export const createUserAction = actionClient
   .inputSchema(createUserSchema)
   .action(async ({ parsedInput }) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
     const user = await createUserUseCase.execute(parsedInput);
     revalidatePath("/users");
     return user;
