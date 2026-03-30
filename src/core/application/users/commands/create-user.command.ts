72becb247959e573237533d39351b2379496b4a7
MaxVzl <maxime.vozelle@gmail.com>
2026-03-30 11:19:24 +0200

    Add create user command schema using Zod validation
    - Introduced `createUserSchema` for validating user creation input, including fields for name, email, and role.
    - Defined `CreateUserCommand` type based on the schema to ensure type safety in user creation operations.

---
+++ src/core/application/users/commands/create-user.command.ts
@@ -0,0 +1,13 @@
+import { Role } from "@/core/domain/users/enums/role.enum";
+import { emailSchema } from "@/core/domain/users/value-objects/email.vo";
+import z from "zod";
+
+export const createUserSchema = z.object({
+  data: z.object({
+    name: z.string(),
+    email: emailSchema,
+    role: z.enum(Role),
+  }),
+});
+
+export type CreateUserCommand = z.infer<typeof createUserSchema>;
