cb525a54a64630b2cacfeaa3877134980f103d40
MV <mv@scrinfo.net>
2026-04-17 16:59:19 +0200

    Add better-auth integration and update authentication utilities
    - Added `better-auth` dependency to the project for enhanced authentication capabilities.
    - Created new authentication handler in `src/app/api/auth/[...all]/route.ts` to manage GET and POST requests.
    - Implemented `auth-client` in `src/lib/auth-client.ts` for client-side authentication.
    - Established authentication configuration in `src/lib/auth.ts` with a base URL.
    - Refactored utility functions in `src/lib/utils.ts` to utilize session management from `better-auth`, enhancing authorization checks.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,34 +1,64 @@
 import { UserDto } from "@/core/application/users/dtos/user.dto";
 import { Role } from "@/core/domain/users/enums/role.enum";
+import { auth } from "@/lib/auth";
+import { authClient } from "@/lib/auth-client";
+import { Session, User } from "better-auth";
 
+// export const withAuth = async <T>(
+//   action: (currentUser: UserDto) => Promise<T>,
+// ): Promise<T> => {
+//   const user: UserDto = {
+//     id: "4ebd1244-bef0-4b01-ba3e-74fae09aeb90",
+//     name: "John Doe",
+//     email: "john.doe@example.com",
+//     role: "admin",
+//   };
+//   return action(user);
+// };
 export const withAuth = async <T>(
-  action: (currentUser: UserDto) => Promise<T>,
+  action: (currentSession: { session: Session; user: User }) => Promise<T>,
 ): Promise<T> => {
-  const user: UserDto = {
-    id: "4ebd1244-bef0-4b01-ba3e-74fae09aeb90",
-    name: "John Doe",
-    email: "john.doe@example.com",
-    role: "admin",
-  };
-  return action(user);
+  const session = await auth.api.getSession();
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+  return action(session);
 };
 
+// export const withRoles = async <T>(
+//   allowedRoles: Role[],
+//   action: (currentUser: UserDto) => Promise<T>,
+// ): Promise<T> => {
+//   const user: UserDto = {
+//     id: "4ebd1244-bef0-4b01-ba3e-74fae09aeb90",
+//     name: "John Doe",
+//     email: "john.doe@example.com",
+//     role: "admin",
+//   };
+
+//   if (!allowedRoles.includes(user.role)) {
+//     throw new Error(
+//       "Accès refusé : Vous n'avez pas les permissions nécessaires.",
+//     );
+//   }
+
+//   return action(user);
+// };
 export const withRoles = async <T>(
   allowedRoles: Role[],
-  action: (currentUser: UserDto) => Promise<T>,
+  action: (currentSession: { session: Session; user: User }) => Promise<T>,
 ): Promise<T> => {
-  const user: UserDto = {
-    id: "4ebd1244-bef0-4b01-ba3e-74fae09aeb90",
-    name: "John Doe",
-    email: "john.doe@example.com",
-    role: "admin",
-  };
+  const session = await auth.api.getSession();
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
 
-  if (!allowedRoles.includes(user.role)) {
+  // if (!allowedRoles.includes(session.user.role)) {
+  if (!allowedRoles.includes((session.user as any).role)) {
     throw new Error(
       "Accès refusé : Vous n'avez pas les permissions nécessaires.",
     );
   }
 
-  return action(user);
+  return action(session);
 };
