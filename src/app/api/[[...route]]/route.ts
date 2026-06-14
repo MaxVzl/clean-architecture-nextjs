463ebc83219b10848f26b7d619ef8de2d68c108c
MaxVzl <maxime.vozelle@gmail.com>
2026-06-14 10:27:44 +0200

    Integrate OpenAPI support in API routes
    - Replaced `Hono` with `OpenAPIHono` to enable OpenAPI documentation for the API.
    - Added a new `/scalar` endpoint for API documentation and defined the OpenAPI specification at `/doc`, enhancing API discoverability and usability.

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,9 +1,20 @@
-import { Hono } from "hono";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { Scalar } from "@scalar/hono-api-reference";
 import { handle } from "hono/vercel";
 import { postsRouter } from "@/core/presentation/routes/posts.routes";
 import { usersRouter } from "@/core/presentation/routes/users.routes";
 
-const app = new Hono().basePath("/api");
+const app = new OpenAPIHono().basePath("/api");
+
+app.get("/scalar", Scalar({ url: "/api/doc" }));
+
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "My API",
+  },
+});
 
 app.get("/hello", (c) => {
   return c.json({
