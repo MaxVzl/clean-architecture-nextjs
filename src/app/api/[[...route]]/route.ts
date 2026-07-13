4cdc0c3b46dd2a22d44d7d6bb42d2c14cf47c878
MV <mv@scrinfo.net>
2026-07-13 16:55:41 +0200

    Refactor API route setup and introduce API client
    - Consolidated API route definitions into a single configuration for improved readability and maintainability.
    - Added a new `api-client.ts` file to facilitate API interactions, utilizing the Hono client with the defined app type.
    - Streamlined the structure of the API routes by removing redundant code and enhancing clarity.

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -4,28 +4,25 @@ import { handle } from "hono/vercel";
 import { postRouter } from "@/core/presentation/routers/post.router";
 import { userRouter } from "@/core/presentation/routers/user.router";
 
-const app = new OpenAPIHono().basePath("/api");
+const app = new OpenAPIHono()
+  .basePath("/api")
+  .doc("/doc", {
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "My API",
+    },
+  })
+  .get("/scalar", Scalar({ url: "/api/doc" }))
+  .get("/hello", (c) => {
+    return c.json({
+      message: "Hello Next.js!",
+    });
+  })
+  .route("/posts", postRouter)
+  .route("/users", userRouter);
 
-app.get("/scalar", Scalar({ url: "/api/doc" }));
-
-app.doc("/doc", {
-  openapi: "3.0.0",
-  info: {
-    version: "1.0.0",
-    title: "My API",
-  },
-});
-
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
-
-// app.use((c, next) => new AuthMiddleware({}).authenticated(c, next))
-
-app.route("/posts", postRouter);
-app.route("/users", userRouter);
+export type AppType = typeof app;
 
 export const GET = handle(app);
 export const POST = handle(app);
