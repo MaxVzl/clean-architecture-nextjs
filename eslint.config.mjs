1e156e9e7b072f6c036a04972b8cddc8a1a6bc59
MaxVzl <maxime.vozelle@gmail.com>
2026-07-14 16:48:44 +0200

    Update ESLint configuration to include unused-imports plugin and enhance rules
    - Added `eslint-plugin-unused-imports` to manage unused imports and improve code quality.
    - Updated ESLint rules to disable the default unused variable check and enforce error reporting for unused imports.
    - Consolidated global ignore patterns for better organization in the ESLint configuration.
    - Modified `package.json` and `pnpm-lock.yaml` to include the new plugin dependency.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,29 +2,27 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import importPlugin from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
   {
     plugins: {
       import: importPlugin,
+      "unused-imports": unusedImports,
     },
     rules: {
-      "@typescript-eslint/no-unused-vars": [
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
         "error",
         {
-          argsIgnorePattern: "^_", // Ignore les arguments qui commencent par _
-          varsIgnorePattern: "^_", // Ignore les variables qui commencent par _
-          caughtErrorsIgnorePattern: "^_", // Ignore les erreurs dans les catch
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
         },
       ],
       "no-undef": "error",
